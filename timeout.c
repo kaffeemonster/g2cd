0f001b5e95f6794c97908ffcf1aff472a703346d
Jan Seiffert <redbully@cc.hs-owl.de>
2009-05-29 00:25:05 +0200

    make timeout stuff usageble
     timeout.h
     timeout.c
    	use a rbtree instead of a timer wheel/sortet list foo
    	adapt code accordingly
    	add a function pointer & data to the timeout
    	add a lock to the timeout
    	finish cancel with the timer lock
    	and handling:
    	- really grovel through the nearest timeouts
    	- lock them
    	- remove them
    	- make main lock free
    	- call timeout function
    	use the nearest timeout as wakeup time
    	add a function to move a timer to a later time
    	use the local_time_now on insert and advance

--- timeout.c
+++ timeout.c
@@ -2,7 +2,7 @@
  * timeout.c
  * thread to handle the varios server timeouts needed
  *
- * Copyright (c) 2004,2008, Jan Seiffert
+ * Copyright (c) 2004-2009 Jan Seiffert
  *
  * This file is part of g2cd.
  *
@@ -43,26 +43,19 @@
 #define DSEC2NSEC (100*1000*1000)
 /* 100 * 0.1sec */
 #define DEFAULT_TIMEOUT	100
-/*  */
-#define TWHEEL_SLOTS 10
 
 /* vars */
 static struct
 {
-	pthread_cond_t	cond;
-	pthread_mutex_t	mutex;
-	struct timespec	time;
+	pthread_cond_t  cond;
+	pthread_mutex_t mutex;
+	struct rb_root tree;
+	struct timespec time;
 	bool	abort;
 } wakeup = {
-	.cond  = PTHREAD_COND_INITIALIZER,
-	.mutex = PTHREAD_MUTEX_INITIALIZER,
+	.cond   = PTHREAD_COND_INITIALIZER,
+	.mutex  = PTHREAD_MUTEX_INITIALIZER,
 };
-
-static struct
-{
-	pthread_mutex_t	mutex;
-	struct list_head	slot;
-} twheel[TWHEEL_SLOTS];
 
 /* helper funcs */
 static always_inline unsigned int to2sec(unsigned int timeout)
@@ -78,67 +71,6 @@ static always_inline unsigned long to2nsec(unsigned int timeout)
 static always_inline unsigned long usec2nsec(unsigned long usec)
 {
 	return usec * 1000;
-}
-
-static always_inline int delta2index(long delta)
-{
-#ifndef I_HATE_CBRANCH
-	int res;
-
-	if(delta <= 0)
-		res = 0;
-	else if(delta <= 5)
-		res = 1;
-	else if(delta <= 12)
-		res = 2;
-	else if(delta <= 25)
-		res = 3;
-	else if(delta <= 45)
-		res = 4;
-	else if(delta <= 60)
-		res = 5;
-	else if(delta <= 120)
-		res = 6;
-	else if(delta <= 180)
-		res = 7;
-	else if(delta <= 300)
-		res = 8;
-	else
-		res = 9;
-
-	return res;
-#else
-	/* 
-	 * PPC doesn't like conditional jumps and lots of
-	 * cmps are also harmfull (updating FLAGS is microcoded
-	 * and microcode is bad implemented...)
-	 */
-# define BPL (sizeof(long)*BITS_PER_CHAR)
-	unsigned long x_lt;
-	unsigned long res;
-
-	x_lt = (unsigned long)((delta - 301) >> (BPL-1));
-	res = (x_lt & 8) | (~x_lt & 9);
-	x_lt = (unsigned long)((delta - 181) >> (BPL-1));
-	res = (x_lt & 7) | (~x_lt & res);
-	x_lt = (unsigned long)((delta - 121) >> (BPL-1));
-	res = (x_lt & 6) | (~x_lt & res);
-	x_lt = (unsigned long)((delta -  61) >> (BPL-1));
-	res = (x_lt & 5) | (~x_lt & res);
-	x_lt = (unsigned long)((delta -  46) >> (BPL-1));
-	res = (x_lt & 4) | (~x_lt & res);
-	x_lt = (unsigned long)((delta -  26) >> (BPL-1));
-	res = (x_lt & 3) | (~x_lt & res);
-	x_lt = (unsigned long)((delta -  13) >> (BPL-1));
-	res = (x_lt & 2) | (~x_lt & res);
-	x_lt = (unsigned long)((delta -   6) >> (BPL-1));
-	res = (x_lt & 1) | (~x_lt & res);
-	x_lt = (unsigned long)((delta -   1) >> (BPL-1));
-	res = (x_lt & 0) | (~x_lt & res);
-
-	return (int) res;
-# undef BPL
-#endif
 }
 
 static always_inline long timespec_delta_sec(struct timespec *a, struct timespec *b)
@@ -164,6 +96,22 @@ static always_inline bool timespec_after(struct timespec *a, struct timespec *b)
 	return false;
 }
 
+static always_inline int timespec_cmp(struct timespec *a, struct timespec *b)
+{
+	int ret = a->tv_sec - b->tv_sec;
+	if(ret)
+		return ret;
+	if((ret = a->tv_nsec - b->tv_nsec))
+		return ret;
+	return (int)(a - b);
+}
+
+static always_inline void timespec_fill_local(struct timespec *t)
+{
+	t->tv_sec = local_time_now;
+	t->tv_nsec = 0;
+}
+
 static always_inline int timespec_fill(struct timespec *t)
 {
 	struct timeval x;
@@ -185,51 +133,111 @@ static always_inline void timespec_add(struct timespec *t, unsigned int timeout)
 	t->tv_nsec += to2nsec(timeout);
 }
 
-/* handling funcs */ 
-bool timeout_add(struct timeout *new_timeout, unsigned int timeout)
+static struct timeout *timeout_nearest(void)
 {
-	struct timespec now;
-	struct list_head *p;
-	int i;
+	struct rb_node *n = rb_first(&wakeup.tree);
+	if(!n)
+		return NULL;
+	return rb_entry(n, struct timeout, rb);
+}
 
-	if(timespec_fill(&now))
-		return false;
-	new_timeout->t = now;
-	timespec_add(&new_timeout->t, timeout);
+static bool timeout_rb_insert(struct timeout *t)
+{
+	struct rb_node **p = &wakeup.tree.rb_node;
+	struct rb_node *parent = NULL;
 
-	i = delta2index(timespec_delta_sec(&new_timeout->t, &now));
-
-	if(pthread_mutex_lock(&twheel[i].mutex))
-		return false;
-
-	if(!list_empty(&twheel[i].slot))
+	while(*p)
 	{
-		list_for_each(p, &twheel[i].slot)
-		{
-			struct timeout *e = list_entry(p, struct timeout, l);
-			if(timespec_after(&e->t, &new_timeout->t))
-			{
-				list_add_tail(&new_timeout->l, p);
-				break;
-			}
-		}
-	}
-	else
-		list_add(&new_timeout->l, &twheel[i].slot);
+		struct timeout *n = rb_entry(*p, struct timeout, rb);
+		int result = timespec_cmp(&t->t, &n->t);
 
-	if(unlikely(pthread_mutex_unlock(&twheel[i].mutex)))
-		diedie("ahhhh, timer wheel stuck!");
+		parent = *p;
+		if(result < 0)
+			p = &((*p)->rb_left);
+		else if(result > 0)
+			p = &((*p)->rb_right);
+		else
+			return false;
+	}
+	rb_link_node(&t->rb, parent, p);
+	rb_insert_color(&t->rb, &wakeup.tree);
 
 	return true;
 }
 
-bool timeout_cancel(struct timeout *who_to_cancel GCC_ATTR_UNUSED_PARAM)
+/* handling funcs */
+bool timeout_add(struct timeout *new_timeout, unsigned int timeout)
 {
+	bool ret_val;
+
+	if(!RB_EMPTY_NODE(&new_timeout->rb))
+		return false;
+
+	timespec_fill_local(&new_timeout->t);
+	timespec_add(&new_timeout->t, timeout);
+
+	barrier();
+	pthread_mutex_lock(&wakeup.mutex);
+
+	ret_val = timeout_rb_insert(new_timeout);
+	if(ret_val && !timespec_after(&new_timeout->t, &wakeup.time))
+		pthread_cond_broadcast(&wakeup.cond);
+
+	pthread_mutex_unlock(&wakeup.mutex);
+
+	return ret_val;
+}
+
+bool timeout_advance(struct timeout *timeout, unsigned int advancement)
+{
+	bool ret_val = false;
 	struct timespec now;
 
-	timespec_fill(&now);
+	timespec_fill_local(&now);
+	timespec_add(&now, advancement);
 
-	return true;
+	barrier();
+	pthread_mutex_lock(&wakeup.mutex);
+
+	if(0 != pthread_mutex_trylock(&timeout->lock))
+		goto out_unlock;
+
+	if(RB_EMPTY_NODE(&timeout->rb)) {
+		pthread_mutex_unlock(&timeout->lock);
+		goto out_unlock;
+	}
+	rb_erase(&timeout->rb, &wakeup.tree);
+
+	pthread_mutex_unlock(&timeout->lock);
+
+	timeout->t = now;
+
+	ret_val = timeout_rb_insert(timeout);
+
+	if(ret_val && !timespec_after(&timeout->t, &wakeup.time))
+		pthread_cond_broadcast(&wakeup.cond);
+
+out_unlock:
+	pthread_mutex_unlock(&wakeup.mutex);
+
+	return ret_val;
+}
+
+bool timeout_cancel(struct timeout *who_to_cancel)
+{
+	bool ret_val = false;
+
+	pthread_mutex_lock(&wakeup.mutex);
+	pthread_mutex_lock(&who_to_cancel->lock);
+	if(!RB_EMPTY_NODE(&who_to_cancel->rb)) {
+		rb_erase(&who_to_cancel->rb, &wakeup.tree);
+		RB_CLEAR_NODE(&who_to_cancel->rb);
+		ret_val = true;
+	}
+	pthread_mutex_unlock(&who_to_cancel->lock);
+	pthread_mutex_unlock(&wakeup.mutex);
+
+	return ret_val;
 }
 
 void timeout_timer_task_abort(void)
@@ -238,38 +246,72 @@ void timeout_timer_task_abort(void)
 	pthread_cond_broadcast(&wakeup.cond);
 }
 
+static void kick_timeouts(void)
+{
+	struct timespec now;
+	struct timeout *t;
+
+	for(timespec_fill(&now), t = timeout_nearest();
+	    t && !timespec_after(&t->t, &now);
+	    timespec_fill(&now), t = timeout_nearest())
+	{
+		pthread_mutex_lock(&t->lock);
+
+		if(!RB_EMPTY_NODE(&t->rb)) {
+			rb_erase(&t->rb, &wakeup.tree);
+			RB_CLEAR_NODE(&t->rb);
+		} else {
+			pthread_mutex_unlock(&t->lock);
+			continue;
+		}
+
+		pthread_mutex_unlock(&wakeup.mutex);
+
+		if(t->fun)
+			t->fun(t->data);
+		else
+			logg_devel("empty timeout??\n");
+
+		pthread_mutex_unlock(&t->lock);
+		pthread_mutex_lock(&wakeup.mutex);
+	}
+}
+
 void *timeout_timer_task(void *param GCC_ATTR_UNUSED_PARAM)
 {
-	int i;
-	for(i = 0; i < TWHEEL_SLOTS; i++)
-		INIT_LIST_HEAD(&twheel[i].slot);
-
 	logg(LOGF_DEBUG, "timeout_timer:\trunning\n");
 
 	server.status.all_abord[THREAD_TIMER] = true;
 
 	g2_set_thread_name(OUR_PROC " timeout");
 
+	pthread_mutex_lock(&wakeup.mutex);
 	while(!wakeup.abort)
 	{
+		struct timeout *t;
 		int ret_val;
 
-		pthread_mutex_lock(&wakeup.mutex);
-
-		timespec_fill(&wakeup.time);
-		timespec_add(&wakeup.time, DEFAULT_TIMEOUT);
+		t = timeout_nearest();
+		if(t)
+			wakeup.time = t->t;
+		else
+		{
+			timespec_fill(&wakeup.time);
+			timespec_add(&wakeup.time, DEFAULT_TIMEOUT);
+		}
 
 		ret_val = pthread_cond_timedwait(&wakeup.cond, &wakeup.mutex, &wakeup.time);
-		pthread_mutex_unlock(&wakeup.mutex);
-
 		/* someone woken us */
 		if(!ret_val)
 		{
 			logg_devel("woken\n");
+			if(wakeup.abort)
+				break;
 		}
-		else if(ETIMEDOUT == ret_val)
+
+		if(0 == ret_val || ETIMEDOUT == ret_val)
 		{
-			logg_devel_old("timeout\n");
+			kick_timeouts();
 		}
 		else
 		{
@@ -278,6 +320,7 @@ void *timeout_timer_task(void *param GCC_ATTR_UNUSED_PARAM)
 			wakeup.abort = true;
 		}
 	}
+	pthread_mutex_unlock(&wakeup.mutex);
 
 	logg_pos(LOGF_NOTICE, "should go down\n");
 	server.status.all_abord[THREAD_TIMER] = false;	
@@ -285,5 +328,6 @@ void *timeout_timer_task(void *param GCC_ATTR_UNUSED_PARAM)
 	return(NULL);
 }
 
+/*@unused@*/
 static char const rcsid_t[] GCC_ATTR_USED_VAR = "$Id: timeout.c,v 1.14 2005/11/05 09:57:22 redbully Exp redbully $";
-// EOF
+/* EOF */
--- timeout.h
+++ timeout.h
@@ -2,19 +2,19 @@
  * timeout.h
  * header-file for timeout.c
  *
- * Copyright (c) 2004, Jan Seiffert
+ * Copyright (c) 2004-2009 Jan Seiffert
  *
  * This file is part of g2cd.
  *
  * g2cd is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version
  * 2 as published by the Free Software Foundation.
- * 
+ *
  * g2cd is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with g2cd; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
@@ -24,27 +24,43 @@
  */
 
 #ifndef _TIMEOUT_H
-#define _TIMEOUT_H
+# define _TIMEOUT_H
 
-#include <sys/time.h>
-#include "lib/list.h"
+# include <sys/time.h>
+# include <pthread.h>
+# include "lib/rbtree.h"
 
 struct timeout
 {
-	struct list_head l;
+	struct rb_node rb;
 	struct timespec t;
+	pthread_mutex_t lock;
+	void *data;
+	void (*fun)(void *);
 };
 
-#ifndef _TIMEOUT_C
-#define _TOUT_EXTRN(x) extern x GCC_ATTR_VIS("hidden")
-#else
-#define _TOUT_EXTRN(x) x GCC_ATTR_VIS("hidden")
-#endif
+# ifndef _TIMEOUT_C
+#  define TOUT_EXTRN(x) extern x GCC_ATTR_VIS("hidden")
+# else
+#  define TOUT_EXTRN(x) x GCC_ATTR_VIS("hidden")
+# endif
 
-_TOUT_EXTRN(bool timeout_add(struct timeout *, unsigned int));
-_TOUT_EXTRN(bool timeout_cancel(struct timeout *));
-_TOUT_EXTRN(void timeout_timer_task_abort(void));
-_TOUT_EXTRN(void *timeout_timer_task(void *));
+static inline void INIT_TIMEOUT(struct timeout *t)
+{
+	RB_CLEAR_NODE(&t->rb);
+	pthread_mutex_init(&t->lock, NULL);
+}
 
-#endif //_TIMEOUT_H
-//EOF
+static inline void DESTROY_TIMEOUT(struct timeout *t)
+{
+	pthread_mutex_destroy(&t->lock);
+}
+
+TOUT_EXTRN(bool timeout_add(struct timeout *, unsigned int));
+TOUT_EXTRN(bool timeout_advance(struct timeout *, unsigned int));
+TOUT_EXTRN(bool timeout_cancel(struct timeout *));
+TOUT_EXTRN(void timeout_timer_task_abort(void));
+TOUT_EXTRN(void *timeout_timer_task(void *));
+
+#endif /* _TIMEOUT_H */
+/* EOF */
