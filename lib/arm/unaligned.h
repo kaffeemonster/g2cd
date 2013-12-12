469b6291499af43e9068335092e5128945f0ade6
Jan Seiffert <kaffeemonster@users.sourceforge.net>
2013-12-12 22:46:45 +0100

    modern ARM can do unaligned access, so assimilate assimilate...

--- lib/arm/unaligned.h
+++ lib/arm/unaligned.h
@@ -1,8 +1,21 @@
 #ifndef _LIB_ARM_UNALIGNED_H
 # define _LIB_ARM_UNALIGNED_H
 
-# include "../unaligned/le_byteshift.h"
-# include "../unaligned/be_byteshift.h"
+# ifdef __ARM_FEATURE_UNALIGNED
+/*
+ * >= ARMv6 (but not -M) can do unaligned access
+ * (if the bits are set right...).
+ * They also have the rev instruction (like the
+ * x86 bswap).
+ * access_ok.h should be fine, but to give the
+ * compiler a heads up, we use the packed struct
+ */
+#  include "../unaligned/le_struct.h"
+#  include "../unaligned/be_struct.h"
+# else
+#  include "../unaligned/le_byteshift.h"
+#  include "../unaligned/be_byteshift.h"
+# endif
 # include "../unaligned/generic.h"
 
 /* Select endianness */
