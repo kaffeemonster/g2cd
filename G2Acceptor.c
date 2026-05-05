2e02ce38e7e39cb2a734d8c13e6813f572680a03
Jan Seiffert <kaffeemonster@googlemail.com>
2026-05-05 23:21:26 +0200

    fix off-by-one in error path
    all this time... in case the accept socket cant be opened, we read under
    the array. Only now thats crashing, it's not like that never happened
    (two instances running at same time)

--- G2Acceptor.c
+++ G2Acceptor.c
@@ -99,9 +99,8 @@ bool init_accept(some_fd epoll_fd)
 	if(server.settings.bind.use_ip4 && server.settings.bind.ip4.num)
 	{
 		ipv4_ready = true;
-		for(i = 0; ipv4_ready && i < server.settings.bind.ip4.num; i++) {
+		for(i = 0; ipv4_ready && i < server.settings.bind.ip4.num; i++, idx++) {
 			bool res = init_con_a(&accept_sos[idx].fd, &server.settings.bind.ip4.a[i]);
-			idx += !!res;
 			ipv4_ready = ipv4_ready && res;
 		}
 		if(!ipv4_ready)
@@ -115,9 +114,8 @@ bool init_accept(some_fd epoll_fd)
 	if(server.settings.bind.use_ip6 && server.settings.bind.ip6.num)
 	{
 		ipv6_ready = true;
-		for(i = 0; ipv6_ready && i < server.settings.bind.ip6.num; i++) {
+		for(i = 0; ipv6_ready && i < server.settings.bind.ip6.num; i++, idx++) {
 			bool res = init_con_a(&accept_sos[idx].fd, &server.settings.bind.ip6.a[i]);
-			idx += !!res;
 			ipv6_ready = ipv6_ready && res;
 		}
 		if(!ipv6_ready)
